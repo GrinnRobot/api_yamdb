75c41f0b8f438a18854b06b7b82cd381719c1e1b
GrinnRobot <egor.burtovoy4444@gmail.com>
2022-06-11 21:19:56 +0500

    сюр

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,20 +1,20 @@
 import os
 from datetime import timedelta
 
-
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
-
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'users.User'
 
-
+# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -25,10 +25,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'django_filters',
     'api.apps.ApiConfig',
+    'reviews.apps.ReviewsConfig',
     'users.apps.UsersConfig',
-    'reviews',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -58,8 +58,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
@@ -88,6 +87,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 
+# Database
 
 DATABASES = {
     'default': {
@@ -96,6 +96,8 @@ DATABASES = {
     }
 }
 
+
+# Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -113,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-
+# Internationalization
 
 LANGUAGE_CODE = 'en-us'
 
@@ -126,7 +128,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-
+# Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
 
